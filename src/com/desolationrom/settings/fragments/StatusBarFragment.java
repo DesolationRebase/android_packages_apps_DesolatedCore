3b72889daffaf88633b55d0756944dc5303e7279
LorDClockaN <davor@losinj.com>
2015-03-30 16:23:47 +0200

    Bliss logo color settings (2/2)
    Change-Id: I6c8717521f9da9b7ebfa0fa285f51ce9cd639348
    
    Conflicts:
    	res/values/bliss_strings.xml
    	res/xml/status_bar_settings.xml
    	src/com/android/settings/cyanogenmod/StatusBarSettings.java

--- src/com/desolationrom/settings/fragments/StatusBarFragment.java
+++ src/com/desolationrom/settings/fragments/StatusBarFragment.java
@@ -7,10 +7,14 @@ import android.app.*;
 import android.os.*;
 import android.content.*;
 import android.net.Uri;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
-public class StatusBarFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener {
+public class StatusBarFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener, Preference.OnPreferenceChangeListener {
 
         Preference headerImage, clearHeaderImage;
+
+        private static final String KEY_DESO_LOGO_COLOR = "status_bar_deso_logo_color";
+        private ColorPickerPreference mDesoLogoColor;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -21,6 +25,14 @@ public class StatusBarFragment extends PreferenceFragment implements Preference.
 
                 headerImage.setOnPreferenceClickListener(this);
                 clearHeaderImage.setOnPreferenceClickListener(this);
+
+                mDesoLogoColor = (ColorPickerPreference) findPreference(KEY_DESO_LOGO_COLOR);
+                mDesoLogoColor.setOnPreferenceChangeListener(this);
+                int intColor = Settings.System.getInt(getContext().getContentResolver(), Settings.System.STATUS_BAR_DESO_LOGO_COLOR, 0xffffffff);
+                String hexColor = String.format("#%08x", (0xffffffff & intColor));
+                mDesoLogoColor.setSummary(hexColor);
+                mDesoLogoColor.setNewPreviewColor(intColor);
+
 		return super.onCreateView(inflater, container, savedInstanceState);
 	}
 
@@ -33,6 +45,18 @@ public class StatusBarFragment extends PreferenceFragment implements Preference.
                 startActivityForResult(Intent.createChooser(intent, "Select Image"), 1);
             } else if(preference.equals(clearHeaderImage)) {
                 Settings.System.putString(getContext().getContentResolver(), headerImage.getKey(), "");
+            }
+            return true;
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if (preference == mDesoLogoColor) {
+                String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String.valueOf(newValue)));
+                preference.setSummary(hex);
+                int intHex = ColorPickerPreference.convertToColorInt(hex);
+                Settings.System.putInt(getContext().getContentResolver(),
+                        Settings.System.STATUS_BAR_DESO_LOGO_COLOR, intHex);
             }
             return true;
         }
