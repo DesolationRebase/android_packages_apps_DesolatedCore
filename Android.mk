70432388a2777b44b0c96bfba6b1933fb268da40
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-01-25 21:29:44 +0000

    Fix java file inclusion

--- Android.mk
+++ Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := DesolatedCore
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
