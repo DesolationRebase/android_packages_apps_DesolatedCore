e9faae72af5556f7bd8ce44a171561e1ccb3e413
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-01-26 16:21:51 +0000

    Fix switch defaults

--- src/com/desolationrom/settings/widgets/SystemSettingSwitchPreference.java
+++ src/com/desolationrom/settings/widgets/SystemSettingSwitchPreference.java
@@ -17,11 +17,13 @@
 package com.desolationrom.settings.widgets;
 
 import android.content.Context;
+import android.preference.Preference;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.util.AttributeSet;
 
-public class SystemSettingSwitchPreference extends SwitchPreference {
+public class SystemSettingSwitchPreference extends SwitchPreference implements Preference.OnPreferenceClickListener {
+
     public SystemSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
@@ -34,30 +36,26 @@ public class SystemSettingSwitchPreference extends SwitchPreference {
         super(context, null);
     }
 
-    @Override
-    protected boolean persistBoolean(boolean value) {
-        if (shouldPersist()) {
-            if (value == getPersistedBoolean(!value)) {
-                // It's already there, so the same as persisting
-                return true;
-            }
-            Settings.System.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
-        }
-        return false;
+    protected void setValue(boolean value) {
+        Settings.System.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
+    }
+
+
+    protected boolean getValue() {
+        return Settings.System.getInt(getContext().getContentResolver(), getKey(), 0) == 1;
     }
 
     @Override
-    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
-        if (!shouldPersist()) {
-            return defaultReturnValue;
-        }
-		return Settings.System.getInt(getContext().getContentResolver(),
-									  getKey(), defaultReturnValue ? 1 : 0) != 0;
+    protected void onAttachedToActivity() {
+        super.onAttachedToActivity();
+        setChecked(getValue());
+        setOnPreferenceClickListener(this);
     }
 
-    protected boolean isPersisted() {
-        // Using getString instead of getInt so we can simply check for null
-        // instead of catching an exception. (All values are stored as strings.)
-		return Settings.System.getString(getContext().getContentResolver(), getKey()) != null;
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        setValue(isChecked());
+        return true;
     }
+
 }
